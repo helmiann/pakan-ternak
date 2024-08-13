399179c4c6789f45f33054216df0241de87672a4
zuhalhabibi <mohammadzuhalhabibi@gmail.com>
2024-08-13 20:22:49 +0700

    test db

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-echo "test"
+
+$servername = "roundhouse.proxy.rlwy.net";
+$username = "root";
+$password = "gRZYLrXkeQKNjTYtScYsUrmZkiAVhYFI";
+$port = 10557;
+$dbname = "railway";
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+if($conn->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "select id, nama, harga from testtable";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["nama"]. " " . $row["harga"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+
+
+
 ?>
